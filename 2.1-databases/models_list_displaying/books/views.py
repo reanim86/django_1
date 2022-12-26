f548d97119bb5e95bed3a1eb754fc85b6c9336a0
Aleksei Kudinov <reanim86@yandex.ru>
2022-12-27 00:41:02 +0300

    Lection2 task2 add books/<pub_date>/

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
     context = {}
     return render(request, template, context)
+
+def books(request):
+    template = 'books/books.html'
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, template, context)
+
+def books_pub_date(request, pub_date):
+    books = Book.objects.order_by('pub_date')
+    paginator = Paginator(books, 1)
+    n = 1
+    for book in books:
+        if str(book.pub_date) == pub_date:
+            break
+        n += 1
+    page = paginator.get_page(n)
+    template = 'books/books_pub_date.html'
+    context = {'books': page,
+               'page': page}
+    return render(request, template, context)
+
+
