4db1d74da12f7532010b86ad2833576c6e5dcd17
Aleksei Kudinov <reanim86@yandex.ru>
2023-01-23 16:35:08 +0300

    Lection5 add model Sensor

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Имя')
+    description = models.CharField(max_length=30, required=False, verbose_name='Описание')
