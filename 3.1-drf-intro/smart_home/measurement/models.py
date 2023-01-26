123f2a7ca2876e92d135ce72b43d4e14f4d72853
Aleksei Kudinov <reanim86@yandex.ru>
2023-01-26 14:14:50 +0300

    Lection5 done

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -8,3 +8,5 @@ class Measurement(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
     temperature = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(blank=True, null=True, verbose_name='Фото')
+
