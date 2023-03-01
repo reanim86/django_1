852a525c0a10b9f4339ec439810de95cbf7fd6ab
Aleksei Kudinov <reanim86@yandex.ru>
2023-03-01 18:48:33 +0300

    Ltction8 add fixture

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,35 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from model_bakery import baker
+
+from rest_framework.test import APIClient
+
+from students.models import Student, Course
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+
+    return factory
+
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+
+    return factory
+
+@pytest.mark.django_db
+def test_get_course(client):
+    course = course_factory(_quantity=1)
+    response = client.get('/courses/')
+    data = response.json()
+    assert data[0] == course[0]
+
