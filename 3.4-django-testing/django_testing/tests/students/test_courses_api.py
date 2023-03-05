4be477fd17ba7ad3ec23365908e1ca3996a385e2
Aleksei Kudinov <reanim86@yandex.ru>
2023-03-05 12:53:54 +0300

    Lection8 done 2 test

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,5 +1,7 @@
 import pytest
 from model_bakery import baker
+from random import randint
+
 
 from rest_framework.test import APIClient
 
@@ -27,9 +29,26 @@ def course_factory():
     return factory
 
 @pytest.mark.django_db
-def test_get_course(client):
+def test_get_course(client, course_factory):
     course = course_factory(_quantity=1)
-    response = client.get('/courses/')
+    response = client.get('/api/v1/courses/')
     data = response.json()
-    assert data[0] == course[0]
+    assert data[0]['name'] == course[0].name
+
+@pytest.mark.django_db
+def test_get_list_course(client, course_factory):
+    course = course_factory(_quantity=10)
+    response = client.get('/api/v1/courses/')
+    data = response.json()
+    assert len(course) == len(data)
+
+@pytest.mark.django_db
+def test_id_course(client, course_factory):
+    course = course_factory(_quantity=10)
+    n = randint(0, 9)
+    response = client.get('/api/v1/courses/?id=1')
+    # response = client.get('/api/v1/courses/')
+    data = response.json()
+    assert len(data['id'][0]) == course[0].name
+    # assert len(data) == 1
 
