7cafaeb8fba649001322588d06d0959ff817e3d3
Aleksei Kudinov <reanim86@yandex.ru>
2023-03-05 22:44:33 +0300

    Lection8 done all mandatory task

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -46,9 +46,37 @@ def test_get_list_course(client, course_factory):
 def test_id_course(client, course_factory):
     course = course_factory(_quantity=10)
     n = randint(0, 9)
-    response = client.get('/api/v1/courses/?id=1')
-    # response = client.get('/api/v1/courses/')
+    response = client.get('/api/v1/courses/', {'id': f'{course[n].id}'})
     data = response.json()
-    assert len(data['id'][0]) == course[0].name
-    # assert len(data) == 1
+    assert data[0]['name'] == course[n].name
+
+@pytest.mark.django_db
+def test_name_course(client, course_factory):
+    course = course_factory(_quantity=10)
+    n = randint(0, 9)
+    response = client.get('/api/v1/courses/', {'name': f'{course[n].name}'})
+    data = response.json()
+    assert data[0]['name'] == course[n].name
+
+@pytest.mark.django_db
+def test_create_course(client):
+    count = Course.objects.count()
+    response = client.post('/api/v1/courses/', {'name': 'some_course'})
+    assert Course.objects.count() == count + 1
+
+@pytest.mark.django_db
+def test_patch_course(client, course_factory):
+    course = course_factory(_quantity=1)
+    response_patch = client.patch(f'/api/v1/courses/{course[0].id}/', {'name': 'change_name'})
+    response_get = client.get('/api/v1/courses/', {'id': f'{course[0].id}'})
+    data = response_get.json()
+    assert data[0]['name'] == 'change_name'
+
+@pytest.mark.django_db
+def test_delete_course(client, course_factory):
+    course = course_factory(_quantity=1)
+    response_delete = client.delete(f'/api/v1/courses/{course[0].id}/')
+    # response_get = client.get('/api/v1/courses/', {'id': f'{course[0].id}'})
+    # data = response_get.json()
+    assert response_delete.status_code == 204
 
